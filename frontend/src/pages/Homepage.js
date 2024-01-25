c15f967265ec2ded39933ba8265510f367a2de65
유병욱 <tls19190@naver.com>
2024-01-25 09:57:10 +0900

    Design : Font uploaded

--- frontend/src/pages/Homepage.js
+++ frontend/src/pages/Homepage.js
@@ -1,5 +1,13 @@
 import React, { Fragment } from 'react';
 import logo from "../assets/images/logo.png"
+import styled from 'styled-components';
+
+const H1 = styled.h1`
+  font-family: InterBold;
+  color: white;
+  background-color: black;
+`
+
 
 function Homepage() {
   return (
@@ -7,9 +15,9 @@ function Homepage() {
     <header>
       <img src={logo} alt="경로이탈" />
     </header>
-    <body>
-      <h1>홈페이지</h1>
-    </body>
+      <H1>Mela</H1>
+      <br />
+      <H1>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi voluptatum ab, magnam necessitatibus possimus eum reprehenderit odio quidem blanditiis distinctio nihil quis ut, nisi harum aut saepe repudiandae facilis quas?</H1>
     <footer></footer>
    </Fragment> 
   )
