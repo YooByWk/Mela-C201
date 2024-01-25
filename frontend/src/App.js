b9c85bd31bbf6a091b14419ab048036f9c44b4d6
tlfahd2 <tlfahd2@naver.com>
2024-01-25 17:58:48 +0900

    Feat : signup modal use mui

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,6 +6,7 @@ import { Fragment } from "react";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import Homepage from "./pages/Homepage";
 import Navbar from "./components/Navbar";
+import SignupModal from "./components/SignupModal";
 
 function App() {
   return (
@@ -14,13 +15,15 @@ function App() {
       <BrowserRouter>
       <Routes>
         <Route exact  path='/Home' element={<Homepage />}> </Route>
-        <Route path='/nav' element={<Navbar />}> </Route> 
+        <Route path='/nav' element={<Navbar />}> </Route>
+        <Route path='/user' element={<SignupModal />}></Route> 
         <Route path='*' Component={Homepage} /> 
         <Route path='/nav2' Component={Navbar} />
         </Routes>
         <Link to='/home'>Homepage(실험중인곳)</Link>
         <Link to='/'>기본 경로</Link>
         <Link to='/nav'>Navbar</Link>
+        <Link to='/user'>Signup</Link>
       </BrowserRouter>
         <div className="App">
           {/* <h1>test</h1> */}
