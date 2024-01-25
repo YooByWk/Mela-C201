4536270c49d7da262a6b6ab03d5ea9cfe794b32b
유병욱 <tls19190@naver.com>
2024-01-25 14:23:09 +0900

    Feat : Nav 만들었습니다.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,8 +3,9 @@ import "./App.css";
 import { Reset } from "styled-reset";
 import { Fragment } from "react";
 
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import Homepage from "./pages/Homepage";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
@@ -12,12 +13,17 @@ function App() {
         <Reset />
       <BrowserRouter>
       <Routes>
-        <Route path='/Home' element={<Homepage />}> </Route>
+        <Route exact  path='/Home' element={<Homepage />}> </Route>
+        <Route path='/nav' element={<Navbar />}> </Route> 
+        <Route path='*' Component={Homepage} /> 
+        <Route path='/nav2' Component={Navbar} />
         </Routes>
+        <Link to='/home'>Homepage(실험중인곳)</Link>
+        <Link to='/'>기본 경로</Link>
+        <Link to='/nav'>Navbar</Link>
       </BrowserRouter>
-
         <div className="App">
-          <h1>test</h1>
+          {/* <h1>test</h1> */}
         </div>
     </Fragment>
   );
---
+++ frontend/src/components/Navbar.js
@@ -0,0 +1,92 @@
+import styled from 'styled-components'
+import logo from "../assets/images/logo.png"
+import React, { useState } from 'react';
+import { FaSearch } from "react-icons/fa";
+
+
+const Container = styled.div`
+  background-color: #121212;
+  display: flex;
+  height: 3rem;
+  color: #FFFFFF;
+  font-family: InterMedium;
+  font-size: larger;
+  justify-content: space-between;
+  align-items: center;
+  span {
+    font-size: medium;
+    /* margin-left: 5% ; */
+  }
+  img {
+    height: 3rem;
+    margin-left: 2%;
+    margin-right: 5%;
+
+  }
+  .MenuContainer {
+    display: flex;
+    justify-content: space-evenly;
+    padding: 1%;
+    width: 40%;
+  }
+`
+
+const SearchBar = styled.form`
+  // 최상위
+  position: relative;
+  width: 30%;
+  text-align: center;
+  // search 문구 중앙 정렬
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  // 인풋
+  & > input {
+    background-color: #202C44;
+    min-height: 20px;
+    width: 80%;
+    height: 3vh;
+    border:0;
+    border-radius: 15rem;
+    padding-left: 8%;
+    /* min-width: 80px; */
+    text-align: center;
+    color: white;
+    outline: none;
+  };
+  // Icon 위치 조절
+  .Icon {
+    background-color: #202C44;
+    color: grey;
+    position: absolute;
+    top: 10%;
+    left: 12%;
+    height: 85%;
+  };
+`
+
+const submitHandler = (event) => {
+  event.preventDefault();
+  window.alert('제출버튼확인')
+  // state 
+}
+
+
+function Navbar() {
+  return (
+    <Container>
+      <img  src={logo} alt="" />
+        <SearchBar onSubmit={submitHandler}>
+        <FaSearch className='Icon'/>
+        <input type="search" spellCheck='false' placeholder='Search'  />
+        </SearchBar>
+    <div className='MenuContainer'>
+      <span>Gather</span>
+      <span>Team Space</span>
+      <span>Community</span>
+    </div>
+    </Container>
+  )
+}
+
+export default Navbar
