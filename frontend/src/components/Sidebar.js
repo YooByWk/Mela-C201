071724e64bf1783526291d62438cb3377a36a3e4
kanagyi95 <kanagyi95@gmail.com>
2024-01-26 16:36:07 +0900

    Feat : sidebar 컴포넌트 수정(라이브러리 변경)

--- frontend/src/components/Sidebar.js
+++ frontend/src/components/Sidebar.js
@@ -1,92 +1,48 @@
-import React from 'react';
+import React, { useState, Fragment } from 'react';
+import { Card, List, ListItem, ListItemPrefix } from '@material-tailwind/react';
+
 import { FaRegUser, FaRegHeart } from "react-icons/fa6";
 import { AiOutlineMessage } from "react-icons/ai";
 import { MdOutlineLocalFireDepartment, MdOutlineLogout } from "react-icons/md";
 
-
-const menus = [
-    {
-        title: 'Profile',
-        icon: <FaRegUser />
-    },
-    {
-        title: 'Message',
-        icon: <AiOutlineMessage />
-    },
-    {
-        title: 'Matching',
-        icon: <MdOutlineLocalFireDepartment />
-    },
-    {
-        title: 'Favorite',
-        icon: <FaRegHeart />
-    },
-    {
-        title: 'Logout',
-        icon: <MdOutlineLogout />
-    }
-]
-
 function Sidebar() {
+
     return (
-        <ul role='list'>
-            {menus.map((menu) => (
-                <li key={menu.title} className='flex justify-center'>
-                    <div className='flex'>
-                        <span>
-                            {menu.icon}
-                        </span>
-                    </div>
-                    <div className='flex'>
-                        <p>
-                            {menu.title}
-                        </p>
-                    </div>
-                </li>
-            ))}
-        </ul>
+        <Card className='h-[calc(100vh-2rem)] w-full max-w-[20rem] p-4'>
+            <List>
+                <ListItem>
+                    <ListItemPrefix>
+                        <FaRegUser />
+                    </ListItemPrefix>
+                    Profile
+                </ListItem>
+                <ListItem>
+                    <ListItemPrefix>
+                        <AiOutlineMessage />
+                    </ListItemPrefix>
+                    Message
+                </ListItem>
+                <ListItem>
+                    <ListItemPrefix>
+                        <MdOutlineLocalFireDepartment />
+                    </ListItemPrefix>
+                    Matching
+                </ListItem>
+                <ListItem>
+                    <ListItemPrefix>
+                        <FaRegHeart />
+                    </ListItemPrefix>
+                    Favorite
+                </ListItem>
+                <ListItem>
+                    <ListItemPrefix>
+                        <MdOutlineLogout />
+                    </ListItemPrefix>
+                    Logout
+                </ListItem>
+            </List>
+        </Card>
     )
 }
-
-// import { List, ListItem, ListItemButton, ListItemText } from '@mui/material';
-
-// function Sidebar() {
-//   return (
-//         <nav aria-label='main sidebar'>
-//             <List >
-//                 <ListItem>
-//                     <ListItemButton>
-//                         <FaRegUser/>
-//                         <ListItemText primary='Profile'/>
-//                     </ListItemButton>
-//                 </ListItem>
-//                 <ListItem>
-//                     <ListItemButton>
-//                         <AiOutlineMessage />
-//                         <ListItemText primary='Message'/>
-//                     </ListItemButton>
-//                 </ListItem>
-//                 <ListItem>
-//                     <ListItemButton>
-//                         <MdOutlineLocalFireDepartment />
-//                         <ListItemText primary='Matching'/>
-//                     </ListItemButton>
-//                 </ListItem>
-//                 <ListItem>
-//                     <ListItemButton>
-//                         <FaRegHeart />
-//                         <ListItemText primary='Favorite'/>
-//                     </ListItemButton>
-//                 </ListItem>
-//                 <ListItem>
-//                     <ListItemButton>
-//                         <MdOutlineLogout />
-//                         <ListItemText primary='Logout'/>
-//                     </ListItemButton>
-//                 </ListItem>
-//             </List>
-//         </nav>
-//     );
-// };
 
 export default Sidebar
