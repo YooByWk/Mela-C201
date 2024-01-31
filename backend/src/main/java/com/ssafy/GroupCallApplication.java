8f78a8fb6826cf28643971960f322e1efc941c51
이현호 <lhh9799@naver.com>
2024-01-31 10:56:11 +0900

    test: [Ongoing] Add (Amazon S3) S3Config.java

--- backend/src/main/java/com/ssafy/GroupCallApplication.java
+++ backend/src/main/java/com/ssafy/GroupCallApplication.java
@@ -12,6 +12,10 @@ import java.nio.charset.StandardCharsets;
 
 @SpringBootApplication
 public class GroupCallApplication {
+    //에러 1) java.net.SocketException: Network is unreachable (https://velog.io/@mingsound21/SpringBoot-AWS-S3%EB%A1%9C-%EC%9D%B4%EB%AF%B8%EC%A7%80-%EC%97%85%EB%A1%9C%EB%93%9C)
+    static {
+        System.setProperty("com.amazonaws.sdk.disableEc2Metadata", "true");
+    }
 	public static void main(String[] args) {
         SpringApplication.run(GroupCallApplication.class, args);
     }
