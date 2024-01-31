2deff0308ada4fd36040ff7818b0060e6f7eaab3
kanagyi95 <kanagyi95@gmail.com>
2024-01-31 15:05:27 +0900

    Feat: 회원정보 수정

--- frontend/src/pages/UserUpdate.jsx
+++ frontend/src/pages/UserUpdate.jsx
@@ -1,9 +1,11 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useNavigate } from 'react-router-dom';
 import styled from "styled-components";
 import DefaultButton from "../components/DefaultButton";
 import { MdLockOutline } from "react-icons/md";
 import { CgDanger } from "react-icons/cg";
+import { fetchUser, updateUser } from "../components/API/UserAPI";
+
 
 const Container = styled.div`
     display: flex;
@@ -25,33 +27,90 @@ const Label = styled.span`
 `
 
 const Form = styled.div`
+`
 
+const ProfileImageWrapper = styled.img`
+    width: 50px;
+    height: 50px;
+    border-radius: 100%;
 `
 
 const InputWrapper = styled.div`
     margin-bottom: 1rem;
     display: flex;
+    align-items: center;
     flex-direction: column;
 `
 
 const Input = styled.input`
-    placeholder: ${(props) => props.placeholder};
     background-color: #151c2c;
     color: white;
     border: none;
 `
 
 const ButtonWrapper = styled.div`
-    display: flex;
-    flex-direction: row;
-    padding: 10px;
+    position: absolute;
+    top: 20px;
+    right: 20px;
+    padding: 5px 10px;
+    font-size: 0.8rem;
 `
+
 const Span = styled.span`
     color: white;
     padding: 5px;
 `
 
 function UserUpdateForm(props) {
+    const [values, setValues] = useState({
+        name: '',
+        nickname: '',
+        gender: '',
+        birth: '',
+        searchAllow: ''
+    })
+
+    useEffect(()=> {
+        const getUserInfo = async() => {
+            try {
+                const res = await fetchUser()
+                setValues(res)
+            } catch (err) {
+                console.error(err)
+            }
+        }; getUserInfo()
+       
+    }, [])
+
+    const handleChange = async (e) => {
+        setValues({...values,
+            [e.target.id]: e.target.value
+        })
+    }
+
+    const handleGenderChange = async (e) => {
+        setValues({...values,
+            [e.target.name]: e.target.value
+        })
+    }
+
+    const handleSearchAllowChange = async (e) => {
+        setValues({...values,
+            [e.target.id]: e.target.checked
+        })
+    }
+
+    const handleSubmit = async(e) => {
+        e.preventDefault()
+        try {
+            const updatedUser = await updateUser(values)
+            console.log(updatedUser)
+            navigate('/portfolio')
+        } catch (err) {
+            console.error(err)
+        }
+    }
+
     const navigate = useNavigate()
     
     const handleCancel = () => {
@@ -63,18 +122,31 @@ function UserUpdateForm(props) {
             <Title>
                 Profile Settings
             </Title>
-            <Form>
+            <Form onSubmit={handleSubmit}>
                 <InputWrapper>
+                    <ProfileImageWrapper />
                     <Label>
                         Name
                     </Label>
-                    <Input placeholder={'이름을 입력하세요'}/>
+                    <Input
+                        type="text"
+                        id='name'
+                        value={values.name}
+                        onChange={handleChange}
+                        placeholder="Name"
+                    />
                 </InputWrapper>
                 <InputWrapper>
                     <Label>
                         Nickname
                     </Label>
-                    <Input placeholder={'닉네임을 입력하세요'}/>
+                    <Input 
+                        type="text"
+                        id='nickname'
+                        value={values.nickname}
+                        onChange={handleChange}
+                        placeholder="Nickname"
+                    />
                 </InputWrapper>
                 <InputWrapper>
                     <Label>
@@ -86,31 +158,58 @@ function UserUpdateForm(props) {
                     <Label>
                         Gender
                     </Label>
-
+                    <Input 
+                        type="text"
+                        name='gender'
+                        value={values.gender}
+                        onChange={handleGenderChange}
+                        placeholder="Gender"
+                    />
                 </InputWrapper>
                 <InputWrapper>
                     <Label>
                         Birth
                     </Label>
-
+                    <input 
+                        type="date"
+                        id='birth'
+                        value={values.birth}
+                        onChange={handleChange}
+                        placeholder="생년월일"
+                    />
                 </InputWrapper>
                 <InputWrapper>
                     <Label>
                         Like genre
                     </Label>
-
+                    <Input 
+                        placeholder="선호 장르"
+                    />
                 </InputWrapper>
                 <InputWrapper>
                     <Label>
                         Position
                     </Label>
-                    
+                    <Input 
+                        placeholder="포지션"
+                    />
                 </InputWrapper>
                 <InputWrapper>
                     <Label>
                         SNS
                     </Label>
-                    
+                    <Input />
+                </InputWrapper>
+                <InputWrapper>
+                    <Label>
+                        다른 회원 검색 노출 허용
+                    </Label>
+                    <input 
+                        type="checkbox"
+                        id='searchAllow'
+                        checked={values.searchAllow}
+                        onChange={handleSearchAllowChange}
+                    />
                 </InputWrapper>
             </Form>
             <ButtonWrapper>
@@ -126,6 +225,7 @@ function UserUpdateForm(props) {
                     backgroundcolor={'#254ef8'}
                     fontcolor={'white'}
                     width={'8rem'}
+                    onClick={handleSubmit}
                 />
             </ButtonWrapper>
             <Span>
