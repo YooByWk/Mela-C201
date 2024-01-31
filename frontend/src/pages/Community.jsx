8e36a05c8fdbff57f5903c7f84bbc44bfbb606a0
유병욱 <tls19190@naver.com>
2024-01-31 13:20:23 +0900

    feat :  커뮤니티 게시판

--- frontend/src/pages/Community.jsx
+++ frontend/src/pages/Community.jsx
@@ -1,43 +1,124 @@
 // 커뮤니티 게시판 메인
-import React from 'react';
-import Navbar from '../components/Navbar';
-import styled from 'styled-components';
-import Sidebar from './../components/Sidebar';
-
-// import { mainTheme } from './../styles/theme';
+import React, { useState } from "react";
+import Navbar from "../components/Navbar";
+import styled from "styled-components";
+import Sidebar from "./../components/Sidebar";
+import { FaSearch } from "react-icons/fa";
+import axios from 'axios';
 
 
 const CommunityContainer = styled.div`
-  /* background-color: ${props => props.theme.colours.primary}; */
+  background-color: ${(props) => props.theme.colours.primary};
   display: flex;
   /* flex-direction: column; */
-  height: 70rem;
-  padding-top: 2.5% ;
+  height: 60rem;
+  color: white;
+  padding-top: 3%;
+  justify-content: space-evenly;
+  padding-left: 3%;
 `;
 
-const ComCon = styled.div`
-  color : white;
-  & > hr {
-    border: 3px dashed green;
-    border-color: ${props => props.theme.colours.third};
+const SideDiv = styled.div`
+  width: 10%;
+  padding-left: 2.5%;
+`;
+
+const MainDiv = styled.div`
+  width: 70%;
+  background-color: ${(props) => props.theme.colours.point};
+  border-radius: 50px;
+  overflow: hidden;
+  padding-top: 1%;
+  padding-left: 2%;
+  display: flex;
+  flex-direction: column;
+
+  .Container {
+    margin-top: 5%;
+    h1 {
+      margin-bottom: 3vh;
+    }
+    .SearchContainer {
+      display: flex;
+      align-items: center;
+      input {
+        height: 100%;
+        margin-left: 7.5%;
+        width: 15rem;
+        background-color: ${(props) => props.theme.colours.point};
+        border-top: none;
+        border-right: none;
+        border-left: none;
+        color: white;
+        text-align: center;
+        outline: none;
+        font-size: medium;
+        &:active {
+          border: none;
+        }
+      }
+    }
+    .BoardSearch {
+      display: flex;
+      justify-content: space-between;
+      margin-bottom: 3%;
+      button {
+        margin-right: 20px;
+      }
+    }
   }
-`
-console.log()
+`;
+
+const RSideDiv = styled.div`
+  width: 15%;
+`;
+
+console.log();
+
+//
+const BoardList = () => {
+  const getBoardList = async () => {
+    const response = (await axios.get('http://localhost:8080/recruit')).data
+    console.log(response.data)
+  }
+}
+
+
+// const UserApi = axios.create({
+//   baseURL: 'http://localhost:8080',
+//   headers: {
+//       // 'Content-Type': 'application/json',
+//       'Authorization': `${TOKEN_TYPE} ${ACCESS_TOKEN}`,
+//       'REFRESH_TOKEN': REFRESH_TOKEN,
+//   },
+// })
+
 const Community = () => {
   return (
-    <ComCon>
-        <h1>
-        Community : 이러면 안되는 개발의 표본
-        </h1>
-<hr />
-<hr />
-<br />
-<br />
-<br />
-<h1>테그별 태스트</h1>
-<h2>테그별 태스트</h2>
-<h3>테그별 태스트</h3>
-    </ComCon>
+    <CommunityContainer>
+      <SideDiv>
+        <Sidebar paddingtop="6vh" />
+      </SideDiv>
+      <MainDiv>
+        <Navbar backcolour="10" />
+        <div className="Container">
+          <h1>자유게시판</h1>
+          <div className="BoardSearch">
+            <span className="SearchContainer">
+              <FaSearch />
+              <input type="text" placeholder="검색어를 입력해주세요" />
+            </span>
+            <div>
+              <button>최신순</button>
+              <button>조회수순</button>
+              <button>좋아요순</button>
+            </div>
+          </div>
+          <p>여기에 이제 글 쓰십시오.</p>
+        </div>
+      </MainDiv>
+      <RSideDiv>3</RSideDiv>
+    </CommunityContainer>
   );
 };
 
