c0fc38e9aacb085d1c663ece5851375ee77abfe2
김신영 <estumav@gmail.com>
2024-02-01 15:27:59 +0900

    feat : 구인글 기능 추가 수정 삭제 조회

--- backend/src/main/java/com/ssafy/api/board/service/RecruitService.java
+++ backend/src/main/java/com/ssafy/api/board/service/RecruitService.java
@@ -3,15 +3,21 @@ package com.ssafy.api.board.service;
 import com.ssafy.api.board.request.RecruitGetListReq;
 import com.ssafy.api.board.request.RecruitRegisterPostReq;
 import com.ssafy.api.board.request.RecruitUpdatePutReq;
+import com.ssafy.api.board.response.BoardRecruitRes;
 import com.ssafy.db.entity.Board;
+import com.ssafy.db.entity.BoardRecruit;
+import com.ssafy.db.entity.Position;
 import com.ssafy.db.entity.User;
 
 import java.util.List;
 
 public interface RecruitService {
     void registBoard(RecruitRegisterPostReq registInfo, User user);
-    void updateBoard(RecruitUpdatePutReq updateInfo, Long boardIdx, Long userIdx);
-    void deleteBoard(Long boardIdx);
-    Board getBoard(Long boardIdx) throws Exception;
-    List<Board> getBoardList(RecruitGetListReq getListInfo);
+    void updateBoard(RecruitUpdatePutReq updateInfo, Long recruitIdx, User user);
+    void deleteBoard(Long recruitIdx);
+    BoardRecruitRes getBoard(Long recruitIdx) throws Exception;
+    List<BoardRecruit> getBoardList(RecruitGetListReq getListInfo);
+    void createBoardRecruitPosition(BoardRecruit boardRecruit, List<String> genres);
+    void deleteBoardRecruitPosition(BoardRecruit boardRecruit);
+    List<Position> getPositions(Long recruitIdx);
 }
