4405d7169eeaa05a193ab034118e30fb9da2e5dc
김신영 <estumav@gmail.com>
2024-02-05 11:15:45 +0900

    fix : edit user regist - default userType

--- backend/src/main/java/com/ssafy/api/user/controller/UserController.java
+++ backend/src/main/java/com/ssafy/api/user/controller/UserController.java
@@ -64,7 +64,6 @@ public class UserController {
 
 		//임의로 리턴된 User 인스턴스. 현재 코드는 회원 가입 성공 여부만 판단하기 때문에 굳이 Insert 된 유저 정보를 응답하지 않음.
 		User user = userService.createUser(registerInfo);
-		user.setUserType("unauth");
 
 		return ResponseEntity.status(200).body(BaseResponseBody.of(200, "Success"));
 	}
