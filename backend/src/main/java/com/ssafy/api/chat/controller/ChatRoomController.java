705664434cee51b2c214b2ca50373dc997d5bc77
김신영 <estumav@gmail.com>
2024-02-09 03:45:41 +0900

    refactor : roomidx -> roomid

--- backend/src/main/java/com/ssafy/api/chat/controller/ChatRoomController.java
+++ backend/src/main/java/com/ssafy/api/chat/controller/ChatRoomController.java
@@ -58,12 +58,12 @@ public class ChatRoomController {
 
     @PostMapping("")
     @ApiOperation(value = "채팅방 입장", notes = "유저1과 유저2의 채팅방에 입장한다.")
-    public ResponseEntity<String> createRoom(@ApiIgnore Authentication authentication, @RequestBody Long userIdx2) {
+    public ResponseEntity<String> createRoom(@ApiIgnore Authentication authentication, @RequestBody Long otherUserIdx) {
         SsafyUserDetails userDetails = (SsafyUserDetails)authentication.getDetails();
         String userEmail = userDetails.getUsername();
         User user = userService.getUserByEmail(userEmail);
 
-        String roomIdx = chatRoomService.enterOneToOneRoom(user.getUserIdx(), userIdx2);
+        String roomIdx = chatRoomService.enterOneToOneRoom(user.getUserIdx(), otherUserIdx);
 
         return ResponseEntity.status(200).body(roomIdx);
     }
@@ -76,9 +76,9 @@ public class ChatRoomController {
 //        return ResponseEntity.status(200).body(chatRoomService.findRoomById(roomIdx));
 //    }
 
-    @GetMapping("/{roomidx}")
+    @GetMapping("/{roomid}")
     @ApiOperation(value = "채팅 내역 조회", notes = "roomIdx(채팅방아이디)로 채팅 내역을 불러온다.")
-    public ResponseEntity<List<ChatMessageRes>> chatInfo(@PathVariable(name = "roomidx") String roomIdx) {
+    public ResponseEntity<List<ChatMessageRes>> chatInfo(@PathVariable(name = "roomid") String roomIdx) {
         log.info("채팅내역: 방 아이디: {}", roomIdx);
 
         // 채팅 내역
