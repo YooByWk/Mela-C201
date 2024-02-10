32b03f3d9411f5414210daa024f4cc66469b9eed
kanagyi95 <kanagyi95@gmail.com>
2024-02-10 11:45:29 +0900

    Feat: 채팅 api, css

---
+++ frontend/src/API/ChatAPI.jsx
@@ -0,0 +1,42 @@
+import axios from 'axios'
+
+let ACCESS_TOKEN = localStorage.getItem('accessToken')
+const URL = 'http://localhost:8080/api/v1/chat'
+
+export const ChatAPI = axios.create({
+    baseURL: 'http://localhost:8080/api/v1/chat',
+    headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${ACCESS_TOKEN}`,
+    },
+})
+
+// 모든 채팅방 조회
+export const ChatList = async () => {
+    const response = await ChatAPI.get(`/chatrooms`)
+    return response.data
+}
+
+// 채팅방 입장
+export const EnterChat = async({ otherUserIdx }) => {
+    const response = await ChatAPI.post(`/chatrooms`, {otherUserIdx})
+    return response.data
+}
+
+// 채팅 내역 조회(단일)
+export const SelectChat = async ({ roomid }) => {
+    const response = await ChatAPI.get(`/chatrooms/${roomid}`)
+    return response.data
+}
+
+// 채팅 유저 목록(최신순)
+export const RecentUser = async () => {
+    const response = await ChatAPI.get(`/chatrooms/recentusers`)
+    return response.data
+}
+
+// 팀스페이스 채팅방
+export const teamspaceChat = async ({ teamspaceid }) => {
+    const response = await ChatAPI.get(`/chatrooms/teamspaces/${teamspaceid}`)
+    return response.data
+}
