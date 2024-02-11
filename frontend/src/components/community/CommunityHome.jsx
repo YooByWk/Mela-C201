e44ffa32f6709c86976de7020a1497a25e9c68d3
유병욱 <tls19190@naver.com>
2024-02-12 01:06:00 +0900

    Design : 구인 게시글 리스트 css

--- frontend/src/components/community/CommunityHome.jsx
+++ frontend/src/components/community/CommunityHome.jsx
@@ -1,28 +1,26 @@
 import styled from "styled-components";
 import { FaSearch } from "react-icons/fa";
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 import { BoardList } from "../../API/BoardAPI";
 import { useNavigate, Link } from "react-router-dom";
 import CoSigninModal from "./CoSigninModal";
 import useStore from "../../status/store";
-import DefaultButton from '../DefaultButton';
+import DefaultButton from "../DefaultButton";
 import { GoDotFill } from "react-icons/go";
-import moment from 'moment'
+import moment from "moment";
 import { IoIosArrowBack, IoIosArrowForward } from "react-icons/io";
 import { AiOutlineLike } from "react-icons/ai";
 
-
 function CommunityHome() {
-  const [sortType, setSortType] = useState('')
+  const [sortType, setSortType] = useState("");
   const [currentPage, setCurrentPage] = useState(1);
   const [data, setData] = useState(null);
-  const [boardInput, setBoardInput] = useState('');
-  const movePage = useNavigate()
+  const [boardInput, setBoardInput] = useState("");
+  const movePage = useNavigate();
   const [open, setOpen] = useState(false);
   const [showLoginModal, setShowLoginModal] = useState(false);
   const [totalPageCount, setTotalPageCount] = useState(1);
-  const [likeCount, setLikeCount] = useState()
-
+  const [likeCount, setLikeCount] = useState();
 
   useEffect(() => {
     const fetchData = async () => {
@@ -30,7 +28,7 @@ function CommunityHome() {
       // console.log(response, 'fetch log')
       setData(response.data.boardResList);
       setTotalPageCount(response.data.totalPageCount);
-      setLikeCount(response.data.likeNum)
+      setLikeCount(response.data.likeNum);
       // 패칭한 데이터를 상태에 저장
     };
     fetchData();
@@ -38,193 +36,221 @@ function CommunityHome() {
   }, [currentPage]);
 
   const NextPage = () => {
-    setCurrentPage(prevPage => prevPage +1 )
-  }
+    setCurrentPage((prevPage) => prevPage + 1);
+  };
   const PrevPage = () => {
-    setCurrentPage(prevPage => prevPage > 1 ? prevPage -1 : prevPage)
-  }
+    setCurrentPage((prevPage) => (prevPage > 1 ? prevPage - 1 : prevPage));
+  };
 
   const ViewSorted = async () => {
-    setSortType('view')
-    const response = await BoardList({ page: 1, size: 10, sortKey: "viewNum", word :boardInput? boardInput : '' });
+    setSortType("view");
+    const response = await BoardList({
+      page: 1,
+      size: 10,
+      sortKey: "viewNum",
+      word: boardInput ? boardInput : "",
+    });
     setData(response.data.boardResList);
   };
 
   const LikeSorted = async () => {
-    setSortType('like')
-    const response = await BoardList({ page: 1, size: 10, word : boardInput? boardInput : '', });
-    const sortedData = response.data.boardResList.sort((a, b) => b.likeNum - a.likeNum); 
+    setSortType("like");
+    const response = await BoardList({
+      page: 1,
+      size: 10,
+      word: boardInput ? boardInput : "",
+    });
+    const sortedData = response.data.boardResList.sort(
+      (a, b) => b.likeNum - a.likeNum
+    );
     setData(sortedData);
-    console.log('sortedData: ', sortedData);
+    console.log("sortedData: ", sortedData);
   };
 
   const LastedSorted = async () => {
-    setSortType('latest')
-    const response = await BoardList({ page: 1, size: 10, word :boardInput? boardInput : '' });
-    console.log(response.data.boardResList,'boardresData')
-    if (response.data.boardResList.length>1) {
-      console.log(response.data.length)
+    setSortType("latest");
+    const response = await BoardList({
+      page: 1,
+      size: 10,
+      word: boardInput ? boardInput : "",
+    });
+    console.log(response.data.boardResList, "boardresData");
+    if (response.data.boardResList.length > 1) {
+      console.log(response.data.length);
       setData(response.data.boardResList);
-    } 
+    }
   };
   const islogined = useStore((state) => state.islogined);
-  console.log(islogined,'로그인여부')
+  console.log(islogined, "로그인여부");
 
   const Create = (e) => {
-    e.preventDefault()
-    console.log(islogined,'로그인여부')
-    if (islogined){
-    movePage('/community/create')
-    }
-    else {
-      setShowLoginModal(true)
-      setOpen(true)
+    e.preventDefault();
+    console.log(islogined, "로그인여부");
+    if (islogined) {
+      movePage("/community/create");
+    } else {
+      setShowLoginModal(true);
+      setOpen(true);
     }
   };
 
   const SearchKeyword = async (event) => {
-    setBoardInput(event.target.value)
-    console.log(event.target.value)
-  }
+    setBoardInput(event.target.value);
+    console.log(event.target.value);
+  };
 
-  const SortByKey =  async(event)=> {
-    event.preventDefault()
-    const response =  await BoardList({page:1, size:10, word:boardInput})
-    console.log(response.data.boardResList.length,'sortbykey')
+  const SortByKey = async (event) => {
+    event.preventDefault();
+    const response = await BoardList({ page: 1, size: 10, word: boardInput });
+    console.log(response.data.boardResList.length, "sortbykey");
     if (response.data.boardResList.length > 1) {
-    setData(response.data.boardResList)
-   }
-    else {
-      window.alert('검색 결과가 없습니다!')
+      setData(response.data.boardResList);
+    } else {
+      window.alert("검색 결과가 없습니다!");
     }
-  }
+  };
 
   const BoardClickHandler = async (e) => {
     e.preventDefault();
-    setBoardInput('');
-    const response = await BoardList({ page: 1, size: 10, word :boardInput? boardInput : '' });
-    setData(response.data.boardResList)
-    setSortType('latest')
-  }
+    setBoardInput("");
+    const response = await BoardList({
+      page: 1,
+      size: 10,
+      word: boardInput ? boardInput : "",
+    });
+    setData(response.data.boardResList);
+    setSortType("latest");
+  };
 
   const pages = [];
-  for (let i = 1; i <= Math.ceil(totalPageCount/10); i++) {
+  for (let i = 1; i <= Math.ceil(totalPageCount / 10); i++) {
     pages.push(
       <PaginationButton
         onClick={() => setCurrentPage(i)}
-        isActive={currentPage === i ? 'active' : ''}
-      >
-      </PaginationButton>
+        isActive={currentPage === i ? "active" : ""}
+      ></PaginationButton>
     );
   }
 
-
   return (
     <>
-    <MainDiv>
-      <div className="Container">
-        <h1 onClick={BoardClickHandler}>자유게시판</h1>
-        <div className="BoardSearch">
-          <span className="SearchContainer">
-            <FaSearch />
-            <form  onSubmit={SortByKey}>
-            <input type="text" placeholder="검색어를 입력해주세요" value={boardInput} onChange={SearchKeyword}/>
-            </form>
-          </span>
-          <div>
-            <button onClick={LastedSorted} className={`sort-btn ${sortType === "latest" ? "active-sort" : ""}`}>
-              <GoDotFill />
-              최신순
-            </button>
-            <button onClick={ViewSorted} className={`sort-btn ${sortType === "view" ? "active-sort" : ""}`}>
-              <GoDotFill />
-              조회수순
-            </button>
-            <button onClick={LikeSorted} className={`sort-btn ${sortType === "like" ? "active-sort" : ""}`}>
-              <GoDotFill />
-              인기순
-            </button>
+      <MainDiv>
+        <div className="Container">
+          <h1 onClick={BoardClickHandler}>자유게시판</h1>
+          <div className="BoardSearch">
+            <span className="SearchContainer">
+              <FaSearch />
+              <form onSubmit={SortByKey}>
+                <input
+                  type="text"
+                  placeholder="검색어를 입력해주세요"
+                  value={boardInput}
+                  onChange={SearchKeyword}
+                />
+              </form>
+            </span>
+            <div>
+              <button
+                onClick={LastedSorted}
+                className={`sort-btn ${
+                  sortType === "latest" ? "active-sort" : ""
+                }`}
+              >
+                <GoDotFill />
+                최신순
+              </button>
+              <button
+                onClick={ViewSorted}
+                className={`sort-btn ${
+                  sortType === "view" ? "active-sort" : ""
+                }`}
+              >
+                <GoDotFill />
+                조회수순
+              </button>
+              <button
+                onClick={LikeSorted}
+                className={`sort-btn ${
+                  sortType === "like" ? "active-sort" : ""
+                }`}
+              >
+                <GoDotFill />
+                인기순
+              </button>
+            </div>
+          </div>
+          <div className="header">
+            <span>NO</span>
+            <span>제목</span>
+            <span>작성자</span>
+            <span>날짜</span>
+            <span>조회수</span>
+            <span>추천수</span>
+          </div>
+          <div className="listWrapper">
+            <ul className="content">
+              {data && data.length > 0 ? (
+                data.map((article) => {
+                  return (
+                    <li key={article.boardIdx} className="list">
+                      <div>{article.boardIdx}</div>
+                      <div className="title">
+                        <Link to={`/community/${article.boardIdx}`}>
+                          {article.title.length < 20
+                            ? article.title
+                            : article.title.slice(0, 18) + "..."}
+                        </Link>
+                        <span className="comment-count">
+                          [{article.commentNum}]
+                        </span>
+                      </div>
+                      <div>{article.nickname}</div>
+                      <div>{moment(article.registDate).format("YY-MM-DD")}</div>
+                      <div>{article.viewNum}</div>
+                      <div>
+                        <span>{article.likeNum}</span>
+                      </div>
+                    </li>
+                  );
+                })
+              ) : (
+                <p>게시글이 없습니다.</p>
+              )}
+            </ul>
+          </div>
+          <br />
+          <div className="buttonWrapper">
+            <DefaultButton
+              onClick={Create}
+              text="글쓰기"
+              width="4rem"
+              height="2rem"
+            />
           </div>
         </div>
-        <div className="header">
-          <span>NO</span>
-          <span>제목</span>
-          <span>작성자</span>
-          <span>날짜</span>
-          <span>조회수</span>
-          <span>추천수</span>
+        <div className="footer">
+          <div
+            className="page-btn"
+            onClick={PrevPage}
+            disabled={currentPage === 1}
+          >
+            <IoIosArrowBack />
+          </div>
+          <div className="pagination">{pages}</div>
+          <div className="page-btn" onClick={NextPage}>
+            <IoIosArrowForward />
+          </div>
         </div>
-        <div className="listWrapper">
-          <ul className="content">
-            {data && data.length > 0 ?(
-              data.map((article) => {
-                return (
-                  <li key={article.boardIdx} className="list">
-                    <div>
-                      {article.boardIdx}
-                    </div>
-                    <div className="title">
-                      <Link to={`/community/${article.boardIdx}`}>
-                        {article.title.length < 20
-                          ? article.title
-                          : article.title.slice(0, 18) + '...'}
-                      </Link>
-                      <span className="comment-count">[{article.commentNum}]</span>
-                    </div>
-                    <div>
-                      {article.nickname}
-                    </div>
-                    <div>
-                      {moment(article.registDate).format('YY-MM-DD')}
-                    </div>
-                    <div>
-                      {article.viewNum}
-                    </div>
-                    <div>
-                      <span>{article.likeNum}</span>
-                    </div>
-                  </li>
-                );
-              }) )
-              : (
-                <p>게시글이 없습니다.</p>
-              ) 
-              }
-              
-          </ul>
-        </div>
-<br />
-        <div className="buttonWrapper">
-          <DefaultButton
-            onClick={Create}
-            text='글쓰기'
-            width='4rem'
-            height='2rem'
-          />
-        </div>
-      </div>
-      <div className="footer">
-        <div className="page-btn" onClick={PrevPage} disabled={currentPage === 1}> 
-          <IoIosArrowBack />
-        </div>
-        <div className="pagination">
-          {pages}
-        </div>
-        <div className="page-btn" onClick={NextPage}>
-          <IoIosArrowForward />
-        </div>
-      </div>
-    </MainDiv>
-    {showLoginModal && <CoSigninModal open={open} setOpen={setOpen} />}
+      </MainDiv>
+      {showLoginModal && <CoSigninModal open={open} setOpen={setOpen} />}
     </>
   );
 }
 
 export default CommunityHome;
 
-
 const PaginationButton = styled.button`
-  background-color: ${props => props.isActive ? "#254EF8" : "#13295b"};
+  background-color: ${(props) => (props.isActive ? "#254EF8" : "#13295b")};
   border: none;
   width: 15px;
   height: 15px;
@@ -232,7 +258,7 @@ const PaginationButton = styled.button`
   margin: 3px;
   cursor: pointer;
   align-items: center;
-`
+`;
 
 const MainDiv = styled.div`
   a {
@@ -251,7 +277,7 @@ const MainDiv = styled.div`
   }
 
   .listWrapper {
-    background-color: #202C44;
+    background-color: #202c44;
     border-radius: 10px;
     height: 100%;
     min-height: 2.5rem;
@@ -264,7 +290,7 @@ const MainDiv = styled.div`
     align-items: center;
     justify-content: space-between;
     padding: 10px 0;
-    border-bottom: 1px solid #254EF8;
+    border-bottom: 1px solid #254ef8;
     text-align: center;
   }
 
@@ -277,23 +303,40 @@ const MainDiv = styled.div`
     text-align: center;
   }
 
-  .header span:nth-child(1), .list div:nth-child(1) { flex: 0.5; } /* NO */
-  .header span:nth-child(2), .list div:nth-child(2) { flex: 3; } /* 제목 */
-  .header span:nth-child(3), .list div:nth-child(3) { flex: 1; } /* 작성자 */
-  .header span:nth-child(4), .list div:nth-child(4) { flex: 1; } /* 날짜 */
-  .header span:nth-child(5), .list div:nth-child(5) { flex: 0.5; } /* 조회수 */
-  .header span:nth-child(6), .list div:nth-child(6) { flex: 0.5; } /* 추천수 */
-
+  .header span:nth-child(1),
+  .list div:nth-child(1) {
+    flex: 0.5;
+  } /* NO */
+  .header span:nth-child(2),
+  .list div:nth-child(2) {
+    flex: 3;
+  } /* 제목 */
+  .header span:nth-child(3),
+  .list div:nth-child(3) {
+    flex: 1;
+  } /* 작성자 */
+  .header span:nth-child(4),
+  .list div:nth-child(4) {
+    flex: 1;
+  } /* 날짜 */
+  .header span:nth-child(5),
+  .list div:nth-child(5) {
+    flex: 0.5;
+  } /* 조회수 */
+  .header span:nth-child(6),
+  .list div:nth-child(6) {
+    flex: 0.5;
+  } /* 추천수 */
 
   .sort-btn {
-    color: #6C7383;
-    background-color: #151C2C;
+    color: #6c7383;
+    background-color: #151c2c;
     border: none;
     cursor: pointer;
   }
 
   .sort-btn.active-sort {
-    color: #254EF8;
+    color: #254ef8;
   }
 
   .content {
@@ -320,14 +363,14 @@ const MainDiv = styled.div`
     color: gray;
     margin-left: 10px;
   }
-  
+
   .buttonWrapper {
     position: absolute;
     right: 20px;
   }
 
   .page-btn {
-    background-color: #202C44;
+    background-color: #202c44;
     width: 40px;
     height: 40px;
     border-radius: 100%;
