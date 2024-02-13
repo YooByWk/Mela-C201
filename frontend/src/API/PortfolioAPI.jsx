2f69bc332d4edcf6b2579ed6cce3ea1a903b0320
tlfahd2 <tlfahd2@naver.com>
2024-02-13 15:32:47 +0900

    feat : portfolio search api
    Former-commit-id: e26fcf30ad16eefbf3cdb93de3293cb223d36acb

--- frontend/src/API/PortfolioAPI.jsx
+++ frontend/src/API/PortfolioAPI.jsx
@@ -13,11 +13,17 @@ export const PortfolioAPI = axios.create({
 
 // 포트폴리오 파일 등록
 export const musicUpload = async (formData) => {
-    console.log(formData)
-    for (let key of formData.keys()) {
-        console.log(key, ":", formData.get(key));
-    }
+    // console.log(formData)
+    // for (let key of formData.keys()) {
+    //     console.log(key, ":", formData.get(key));
+    // }
     const response = await PortfolioAPI.post(`/musics`, formData)
-    console.log(response.data)
+    // console.log(response.data)
+    return response.data
+}
+
+// 포트폴리오 검색
+export const musicSearch = async (word) => {
+    const response = await PortfolioAPI.get(`/totalsearchmusic/${word}`)
     return response.data
 }
