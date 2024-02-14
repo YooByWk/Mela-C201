be7a707f5fe87e46f58e6594aa8721ca0e4abe21
유병욱 <tls19190@naver.com>
2024-02-15 01:15:43 +0900

    Feat : FronteEnd Build dir setting js file create
    Former-commit-id: c3821141f6f1a6d188cfe08db60c45a1fe94065c

---
+++ frontend/moveBuild.js
@@ -0,0 +1,16 @@
+const fs = require('fs');
+const path = require('path');
+
+const sourceDir = path.join(__dirname, 'build');
+const destDir = path.join(__dirname, '../backend/src/main/resources/dist');
+
+try {fs.readdirSync(sourceDir).forEach(file => {
+  fs.copyFileSync(path.join(sourceDir, file), path.join(destDir, file));
+});}
+catch (err) {
+  console.log(err,'err');
+}
+
+console.log('Build moved to backend');
+
+fs.rmdirSync(sourceDir, { recursive: true });
