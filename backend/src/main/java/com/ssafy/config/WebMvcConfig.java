d0b74e4b45c6c4aad7cfdbf90e9dd41c01679487
김신영 <estumav@gmail.com>
2024-02-15 23:28:06 +0900

    feat : edit cors
    Former-commit-id: cb173a62230f495082ef17218aed1ebd83d0ba25

--- backend/src/main/java/com/ssafy/config/WebMvcConfig.java
+++ backend/src/main/java/com/ssafy/config/WebMvcConfig.java
@@ -35,30 +35,30 @@ public class WebMvcConfig implements WebMvcConfigurer {
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-    		registry.addResourceHandler("/resources/**")
-    				.addResourceLocations("/WEB-INF/resources/");
-    		
-    		registry.addResourceHandler("swagger-ui.html")
-    				.addResourceLocations("classpath:/META-INF/resources/");
+        registry.addResourceHandler("/resources/**")
+                .addResourceLocations("/WEB-INF/resources/");
 
-    		registry.addResourceHandler("/webjars/**")
-    				.addResourceLocations("classpath:/META-INF/resources/webjars/");
-    		
-    		/*
-    		 * 
-    		 * Front-end에서 참조하는 URL을 /dist로 매핑
-    		 * 
-    		 */
+        registry.addResourceHandler("swagger-ui.html")
+                .addResourceLocations("classpath:/META-INF/resources/");
+
+        registry.addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/");
+
+        /*
+         *
+         * Front-end에서 참조하는 URL을 /dist로 매핑
+         *
+         */
         registry.addResourceHandler("/css/**")
-        			.addResourceLocations("classpath:/dist/css/");
-        	registry.addResourceHandler("/fonts/**")
-        			.addResourceLocations("classpath:/dist/fonts/");
+                .addResourceLocations("classpath:/dist/css/");
+        registry.addResourceHandler("/fonts/**")
+                .addResourceLocations("classpath:/dist/fonts/");
         registry.addResourceHandler("/icons/**")
-				.addResourceLocations("classpath:/dist/icons/");
+                .addResourceLocations("classpath:/dist/icons/");
         registry.addResourceHandler("/img/**")
-			.addResourceLocations("classpath:/dist/img/");
+                .addResourceLocations("classpath:/dist/img/");
         registry.addResourceHandler("/js/**")
-				.addResourceLocations("classpath:/dist/js/");
+                .addResourceLocations("classpath:/dist/js/");
     }
 
     public Filter requestLoggingFilter() {
@@ -75,7 +75,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
     public FilterRegistrationBean loggingFilterRegistration() {
         FilterRegistrationBean registration = new FilterRegistrationBean(requestLoggingFilter());
         registration.addUrlPatterns("/api/**");
-        registration.addUrlPatterns("/ws/**");
+//        registration.addUrlPatterns("/ws/**");
         return registration;
     }
 
